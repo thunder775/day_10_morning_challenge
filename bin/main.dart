a8b8a6a8155ffa9da6bee66d8083a75a2284a527
McLarenCollege <hello@mclarencollege.in>
2019-10-14 08:09:22 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,29 @@
+// Challenge 1
+// Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+// Examples:
+// longestZero("01100001011000") ➞ "0000"
+//
+// longestZero("100100100") ➞ "00"
+//
+// longestZero("11111") ➞ ""
+
+
+// Challenge 2
+// Create a function that performs an even-odd transform to an array, n times. Each even-odd transformation:
+//
+// 1. Adds two (+2) to each odd integer.
+// 2. Subtracts two (-2) to each even integer.
+// Examples:
+// evenOddTransform([3, 4, 9], 3) ➞ [9, -2, 15]
+// Since [3, 4, 9] => [5, 2, 11] => [7, 0, 13] => [9, -2, 15]
+//
+// evenOddTransform([0, 0, 0], 10) ➞ [-20, -20, -20]
+//
+// evenOddTransform([1, 2, 3], 1) ➞ [3, 0, 5]
+
+main() {
+
+}
+
+// Challenge 3
+// Write 2 test cases to test both the previous functions in test.dart
